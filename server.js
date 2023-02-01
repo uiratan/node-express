1562ca042c9f6179be319499e5c890fefe777e65
Uiratan Cavalcante <uiratan@gmail.com>
2023-02-01 12:11:13 -0300

    crud em array finalizado

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require('http');
+import app from './src/app.js'
+
 const port = 3000;
 
-const rotas = {
-  '/': 'Curso de Node',
-  '/livros': 'Entrei na pag de livros',
-  '/autores': 'Listagem de autores',
-  '/sobre': 'Info sobre projeto'
-}
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(rotas[req.url]);
-})
-
-server.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Servidor escutando em http://localhost:${port}`)
 })
