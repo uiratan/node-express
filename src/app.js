d8b4af58537cf444b7d212a6dbef3c7d4ef9162a
Uiratan Cavalcante <uiratan@gmail.com>
2023-02-02 11:00:28 -0300

    conexao com mangoDB criada e endpoit /livro funcionando

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
 import express from "express";
+import db from './config/dbConnect.js';
+import livros from './models/Livro.js';
+
+db.on("error", console.log.bind(console, 'Erro de conexão'));
+db.once("open", () => {
+  console.log('conexão com o banco feita com sucesso');
+})
 
 const app = express();
-
 app.use(express.json());
-
-const livros = [
-  {id: 1, "titulo": "Senhor dos Aneis"},
-  {id: 2, "titulo": "O Hobbit"}
-]
 
 app.get('/', (req, res) => {
   res.status(200).send('Curso de Node');
 }) 
 
 app.get('/livros', (req, res) => {
-  res.status(200).json(livros);
+  livros.find((err, livros) => {
+    res.status(200).json(livros);  
+  });
 })
 
 app.get('/livros/:id', (req, res) => {
@@ -27,8 +30,6 @@ app.post('/livros', (req, res) => {
   res
     .status(201)
     .json(livros);
-    //.send('Livro cadastrado com sucesso.');
-    
 })
 
 app.put('/livros/:id', (req, res) => {
