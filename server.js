2e241607a35ac583306819accdce08472c68c42b
Uiratan Cavalcante <uiratan@gmail.com>
2023-02-07 12:39:59 -0300

    servidor rodando na porta normal

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import app from './src/app.js'
 
-const port = 3000;
+//const port = 3000;
 
-app.listen(port, async () => { 
-   console.log(`Servidor escutando em http://localhost:${port}`)
+// app.listen(port, async () => { 
+//    console.log(`Servidor escutando em http://localhost:${port}`)
+// })
+
+app.listen(async () => { 
+   console.log("Servidor rodando");
 })
